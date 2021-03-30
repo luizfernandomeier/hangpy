8cb1cf75a891b636f88961db491d453807a5e9cc
Luiz Fernando Meier <lfmeier2@gmail.com>
2021-03-30 11:15:54 -0300

    RedisServerRepository tests

---
+++ hangpy/tests/repositories/test_redis_server_repository.py
@@ -0,0 +1,44 @@
+import datetime
+import fakeredis
+import redis
+import unittest
+from freezegun import freeze_time
+from hangpy.entities.server import Server
+from hangpy.repositories.redis_server_repository import RedisServerRepository
+#from hangpy.services import JobActivityBase
+
+class TestRedisServerRepository(unittest.TestCase):
+
+    def test_init (self):
+        redis_client = fakeredis.FakeStrictRedis()
+        server_repository = RedisServerRepository(redis_client)
+        self.assertIsInstance(server_repository.redis_client, redis.StrictRedis)
+
+    @freeze_time('1988-04-10 11:01:02.123456')
+    def test_add_and_get_servers(self):
+        redis_client = fakeredis.FakeStrictRedis()
+        server_repository = RedisServerRepository(redis_client)
+        fake_server = Server()
+        server_repository.add_server(fake_server)
+        actual_server = server_repository.get_servers()[0]
+        expected_server = fake_server
+
+        self.assertNotEqual(actual_server, expected_server)
+        self.assertEqual(actual_server.id, expected_server.id)
+        self.assertEqual(actual_server.start_datetime, expected_server.start_datetime)
+
+    @freeze_time('1988-04-10 11:01:02.123456')
+    def test_update_server(self):
+        redis_client = fakeredis.FakeStrictRedis()
+        server_repository = RedisServerRepository(redis_client)
+        fake_server = Server()
+        server_repository.add_server(fake_server)
+        server = server_repository.get_servers()[0]
+        server.last_cycle_datetime = datetime.datetime.now().isoformat()
+        server_repository.update_server(server)
+        actual_server = server_repository.get_servers()[0]
+
+        self.assertEqual(actual_server.last_cycle_datetime, server.last_cycle_datetime)
+
+if (__name__ == '__main__'):
+    unittest.main()
