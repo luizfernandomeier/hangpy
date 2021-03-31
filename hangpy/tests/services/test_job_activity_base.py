8e778cf7612062fb0592f8a4952c2a2565e49daf
Luiz Fernando Meier <lfmeier2@gmail.com>
2021-03-31 01:13:47 -0300

    JobActivityBase tests

--- hangpy/tests/services/test_job_activity_base.py
+++ hangpy/tests/services/test_job_activity_base.py
@@ -1,25 +1,25 @@
+import hangpy.tests.fake as fake
 import unittest
 from hangpy.services import JobActivityBase
 
 class TestJobActivityBase(unittest.TestCase):
 
+    def test_action(self):
+        fake_abstract_job = fake.FakeAbstractJob()
+        self.assertIsNone(fake_abstract_job.action())
+
     def test_start(self):
-        global job_action_result
-        job_activity = FakeJob()
+        fake.fake_job_action_result = None
+        job_activity = fake.FakeJob()
         job_activity.start()
         expected = "executed the action"
-        self.assertEqual(job_action_result, expected)
+        self.assertEqual(fake.fake_job_action_result, expected)
 
     def test_get_job_object(self):
-        job_activity = FakeJob()
+        job_activity = fake.FakeJob()
         job = job_activity.get_job_object()
-        self.assertTrue(job.module_name.endswith('test_job_activity_base'))
+        self.assertTrue(job.module_name.endswith('fake'))
         self.assertEqual(job.class_name, 'FakeJob')
-
-class FakeJob(JobActivityBase):
-    def action(self):
-        global job_action_result
-        job_action_result = "executed the action"
 
 if (__name__ == "__main__"):
     unittest.main()
